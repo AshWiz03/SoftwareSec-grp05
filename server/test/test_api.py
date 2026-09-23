309e2df5f9cf0b16f4b7134716adb88c9e2bfec8
Manoid <micke.welander@live.se>
2026-09-23 17:02:41 +0200

    Updated
    Updated so script sends http requests instead.

--- server/test/test_api.py
+++ server/test/test_api.py
@@ -1,76 +1,79 @@
-from server import app
 import pytest
+import requests
 
 BASE_URL = "http://localhost:5000"
 rmap_initiate_endpoint = "/api/rmap-initiate"
 rmap_getLink_endpoint = "/api/rmap-get-link"
 #CLIENT
 @pytest.fixture
-def client():
-    app.config["TESTING"] = True
-    return test_all_rmap_invalid_requests(app.test_client())
+#def client():
+    #app.config["TESTING"] = True
+    #return app.test_client()
 
 def test_healthz_route():
-    client = app.test_client()
-    resp = client.get("/healthz")
+    resp = requests.get(f"{BASE_URL}/healthz")
+    #client = app.test_client()
+    #resp = client.get("/healthz")
 
     assert resp.status_code == 200
-    assert resp.is_json
+    assert "application/json" in resp.headers.get("Content-Type", "") # IS JSON
+    assert resp.json()["message"] == "The server is up and running."
     
 
 ## RMAP INITIATE ##
 
 #TEST EMPTY BODY
-def test_rmap_initiate_missing_body(client):
-    resp = client.post("/api/rmap-initiate")
+def test_rmap_initiate_missing_body():
+    resp = requests.post(f"{BASE_URL}/api/rmap-initiate")
 
     assert resp.status_code == 400
-    assert resp.is_json
-    assert resp.get_json()["error"] == "missing payload"
+    assert "application/json" in resp.headers.get("Content-Type", "")
+    assert resp.json()["error"] == "missing payload"
 
 ##TEST EMPTY JSON
-def test_rmap_initiate_empty_json(client):
-    resp = client.post("/api/rmap-initiate", json={})
+def test_rmap_initiate_empty_json():
+    resp = requests.post(f"{BASE_URL}/api/rmap-initiate", json={})
 
     assert resp.status_code == 400
-    assert resp.get_json()["error"] == "missing payload"
+    assert "application/json" in resp.headers.get("Content-Type", "")
+    assert resp.json()["error"] == "missing payload"
 #TEST PAYLOAD IS NONE
-def test_rmap_initiate_payload_is_none(client):
-    resp = client.post(
-        "/api/rmap-initiate",
+def test_rmap_initiate_payload_is_none():
+    resp = requests.post(
+        f"{BASE_URL}/api/rmap-initiate",
         json={"payload": None}
     )
 
     assert resp.status_code == 400
-    assert resp.is_json
-    assert resp.get_json()["error"] == "invalid request"
+    assert "application/json" in resp.headers.get("Content-Type", "")
+    assert resp.json()["error"] == "invalid request"
 #TEST EMPTY PAYLOAD
-def test_rmap_initiate_payload_is_empty(client):
-    resp = client.post(
-        "/api/rmap-initiate",
+def test_rmap_initiate_payload_is_empty():
+    resp = resp = requests.post(
+        f"{BASE_URL}/api/rmap-initiate",
         json={"payload": ""}
     )
 
     assert resp.status_code == 400
-    assert resp.is_json
-    assert resp.get_json()["error"] == "invalid request"
+    assert "application/json" in resp.headers.get("Content-Type", "")
+    assert resp.json()["error"] == "invalid request"
 #TEST INVALID PAYLOAD
-def test_rmap_initiate_invalid_payload(client):
-    resp = client.post(
-        "/api/rmap-initiate",
+def test_rmap_initiate_invalid_payload():
+    resp = requests.post(
+        f"{BASE_URL}/api/rmap-initiate",
         json={"payload": "this-is-not-a-valid-rmap-message"}
     )
 
     assert resp.status_code == 400
-    assert resp.is_json
-    assert resp.get_json()["error"] == "invalid request"
+    assert "application/json" in resp.headers.get("Content-Type", "")
+    assert resp.json()["error"] == "invalid request"
 
 
 
 #TEST CALL
-def test_all_rmap_invalid_requests(client):
-    test_rmap_initiate_missing_body(client)
-    test_rmap_initiate_empty_json(client)
-    test_rmap_initiate_payload_is_none(client)
-    test_rmap_initiate_payload_is_empty(client)
-    test_rmap_initiate_invalid_payload(client)
+#def test_all_rmap_invalid_requests(client):
+ #   test_rmap_initiate_missing_body(client)
+   # test_rmap_initiate_empty_json(client)
+   # test_rmap_initiate_payload_is_none(client)
+   # test_rmap_initiate_payload_is_empty(client)
+   # test_rmap_initiate_invalid_payload(client)
